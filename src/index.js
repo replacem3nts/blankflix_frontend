2fe6b73dfaf10948aceda06919c3fca93a38a9d2
replacem3nts <donny.landis@gmail.com>
2020-06-04 22:02:36 -0400

    builds header including authform button and adds some basic styling

--- src/index.js
+++ src/index.js
@@ -7,7 +7,15 @@ import { BrowserRouter } from 'react-router-dom'
 import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from './reducers/rootReducer'
 
-let store = configureStore({reducer: rootReducer})
+let store = configureStore({
+  reducer: rootReducer,
+  preloadedState: {
+    id: 1,
+    username: 'coolhandluke',
+    token: '',
+    sitename: 'Surfer'
+  }
+})
 
 ReactDOM.render(
   <Provider store={store}>
