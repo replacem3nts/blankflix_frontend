45f2147d5fb1a63ffc5a4badc51bc56b0a57192d
replacem3nts <donny.landis@gmail.com>
2020-06-04 22:27:52 -0400

    builds auth form

--- src/components/loggedout/AuthForm.js
+++ src/components/loggedout/AuthForm.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import { useState } from 'react'
 
-export const AuthForm = () => {
+export const AuthForm = ({formName, handleSubmit}) => {
+    let [username, setUsername] = useState('')
+    let [password, setPassword] = useState('')
+
+    let handleFormSubmit = (e) => {
+        e.preventDefault()
+        let credentials = {username, password}
+        console.log(credentials)
+    }
+
+    let handleChange = ({target}) => {
+        let {name, value} = target
+        name === 'username' ? setUsername(value) : setPassword(value)
+    }
+
     return (
-        <div>
-            HELLO
-        </div>
+        <article>
+            <section>
+                <form onSubmit={handleFormSubmit}>
+                    <h3>{formName}</h3>
+                    <label htmlFor={username} hidden={true}>Username: </label>
+                    <input type='text' autoComplete='off' name='username' value={username} onChange={handleChange}></input><br/>
+                    <label htmlFor={password} hidden={true}>Password: </label>
+                    <input type='text' autoComplete='off' name='password' value={password} onChange={handleChange}></input><br/>
+                    <input type='submit' value='SUBMIT'/>
+                </form>
+            </section>
+        </article>
     )
 }
